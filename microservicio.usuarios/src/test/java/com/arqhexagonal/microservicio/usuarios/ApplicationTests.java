f46968abe56a7f52d9c4a39bac7b3e82276cc6f4
Rommel V1 <rommelkd36@gmail.com>
2025-06-15 12:12:05 -0600

    cambio 2

--- microservicio.usuarios/src/test/java/com/arqhexagonal/microservicio/usuarios/ApplicationTests.java
+++ microservicio.usuarios/src/test/java/com/arqhexagonal/microservicio/usuarios/ApplicationTests.java
@@ -2,12 +2,13 @@ package com.arqhexagonal.microservicio.usuarios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class ApplicationTests {
+@ActiveProfiles("test")
+public class ApplicationTests {
+@Test
+    void  contextLoads(){
 
-	@Test
-	void contextLoads() {
-	}
-
+}
 }
